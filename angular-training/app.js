8a0278b8b7cae3e6050b4e0a18d35560aa4e4360
zaxaar <zahar1982@gmail.com>
2017-08-18 11:00:58 +0200

    play with basic controllers

---
+++ angular-training/app.js
@@ -0,0 +1,37 @@
+var app = angular.module('trainingApp', []);
+
+app.controller('firstController', function($scope){
+  $scope.message = "First Controller Message!";
+});
+
+app.controller('childController', function($scope, $filter, limitToFilter) {
+  $scope.message = "Child Controller Message!";
+
+  $scope.showMessage = function() {
+    alert($scope.message);
+  }
+
+  $scope.showShortMessage = function() {
+    alert(limitToFilter($scope.message, 5));
+  }
+
+  $scope.showCapitalizedMessage = function() {
+    alert($filter('uppercase')($scope.message));
+  }
+
+  $scope.colors = [
+    'black', 'brown', 'blue'
+  ];
+
+  $scope.obj = {id: 5, login: 'admin'};
+  $scope.users = [
+    {name: 'User One'},
+    {name: 'User Two'},
+    {name: 'User Three'}
+  ];
+
+  $scope.showModel = function(){
+    alert($filter('uppercase')($scope.napis));
+  }
+
+});
