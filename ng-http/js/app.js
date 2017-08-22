c920722546475de427a6bd82577e5a585dcd8a36
zaxaar <zahar1982@gmail.com>
2017-08-22 14:31:06 +0200

    add  & delete users - $http POST / DELETE

--- ng-http/js/app.js
+++ ng-http/js/app.js
@@ -19,7 +19,7 @@ app.config(function($routeProvider){
   })
 });
 
-app.controller('firstController', function($scope, $http) {
+app.controller('firstController', function($scope, $http, $filter) {
   $http({
     method: "GET",
     url: "http://localhost:3000/users",
@@ -29,6 +29,20 @@ app.controller('firstController', function($scope, $http) {
   }, function(error) {
     console.error(error);
   });
+
+  $scope.deleteUser = function(id) {
+    $http({
+      url: "http://localhost:3000/users/" + id,
+      method: "DELETE"
+    }).then(function(success){
+      console.log('delete success!', success);
+      var deletedUser = $filter('filter')($scope.users, {id: id})[0];
+      $scope.users.splice($scope.users.indexOf(deletedUser),1);
+    }, function(error) {
+      console.log('delete error', error);
+    });
+  }
+
 })
 
 app.controller('secondController', function($scope, $http, $routeParams, $location) {
@@ -56,4 +70,26 @@ app.controller('thirdController', function($scope, $http) {
   }, function(error) {
     console.warn(error)
   })
+})
+
+app.controller('fourthController', function($scope, $http) {
+
+  $scope.addUser = function() {
+    $http({
+      url: 'http://localhost:3000/users',
+      method: 'POST',
+      data: {
+        name: $scope.name,
+        lastname: $scope.lastname
+      }
+    }).then(function(success){
+      console.log(success.data);
+      $scope.users.push(success.data);
+      $scope.name = '';
+      $scope.lastname = '';
+    }), function(error){
+      console.log(error);
+    }
+  }
+
 })
