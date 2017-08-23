03a3325272218dfc5a22323728e2435fc430174e
zaxaar <zahar1982@gmail.com>
2017-08-23 14:51:40 +0200

    random users generator

---
+++ ng-random-users/js/app.js
@@ -0,0 +1,44 @@
+var app = angular.module('randomUserApp', []);
+var ajaxUrl = 'http://localhost:3000/db/';
+
+app.controller('mainController', function($scope, $http) {
+
+  $scope.resources = {};
+  $scope.users = [];
+  $scope.numberOfUsers = 10;
+
+  $scope.makeUser = function(firstname, lastname){
+    return {firstName: firstname, lastName: lastname}
+  }
+
+  $scope.displayUsers  = function(sex) {
+    $scope.users = [];
+    for(var i = 0; i < $scope.numberOfUsers; i++) {
+      var userSex = sex;
+
+      if(userSex === 'all') {
+        var r = Math.round(Math.random()); // <0, 1>
+        (r === 0) ? userSex = 'male' : userSex = 'female';
+      }
+
+      var userNames = $scope.resources[userSex + '_name'];
+      var userLastNames = $scope.resources[userSex + '_lastname'];
+      var userFirstName = userNames[Math.ceil(Math.random() * userNames.length) - 1];
+      var userLastName = userLastNames[Math.ceil(Math.random() * userLastNames.length) - 1];
+
+      $scope.users.push($scope.makeUser(userFirstName, userLastName));
+    }
+  }
+
+  $http({
+    url: ajaxUrl,
+    method: 'GET'
+  }).then(function(succes){
+    $scope.resources = succes.data;
+    console.log('resources: ', $scope.resources);
+  }, function(error){
+    console.log(error);
+  })
+
+// Main Controller ends here....
+})
